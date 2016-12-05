f9af23ba4084d36eb829ebac8e55329e2d6b6f89
Krzysztof Grzeslak <Krzysztof.grzeslak@WP.pl>
2016-12-05 22:21:57 +0100

    Add files via upload

---
+++ NumberConverter/UserInterface.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace NumberConverter
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DecimalValue = new System.Windows.Forms.TextBox();
+            this.BinaryValue = new System.Windows.Forms.TextBox();
+            this.OctalValue = new System.Windows.Forms.TextBox();
+            this.HexValue = new System.Windows.Forms.TextBox();
+            this.ValueType = new System.Windows.Forms.ComboBox();
+            this.CalculateBttn = new System.Windows.Forms.Button();
+            this.StatusReport = new System.Windows.Forms.Label();
+            this.ClearBttn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(18, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Decimal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(18, 88);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Octal";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(18, 124);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Hexadecimal";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(18, 52);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Binary";
+            // 
+            // DecimalValue
+            // 
+            this.DecimalValue.Location = new System.Drawing.Point(190, 16);
+            this.DecimalValue.Margin = new System.Windows.Forms.Padding(4);
+            this.DecimalValue.MaxLength = 20;
+            this.DecimalValue.Name = "DecimalValue";
+            this.DecimalValue.Size = new System.Drawing.Size(520, 26);
+            this.DecimalValue.TabIndex = 4;
+            // 
+            // BinaryValue
+            // 
+            this.BinaryValue.Location = new System.Drawing.Point(190, 52);
+            this.BinaryValue.Margin = new System.Windows.Forms.Padding(4);
+            this.BinaryValue.MaxLength = 64;
+            this.BinaryValue.Name = "BinaryValue";
+            this.BinaryValue.Size = new System.Drawing.Size(520, 26);
+            this.BinaryValue.TabIndex = 5;
+            // 
+            // OctalValue
+            // 
+            this.OctalValue.Location = new System.Drawing.Point(190, 88);
+            this.OctalValue.Margin = new System.Windows.Forms.Padding(4);
+            this.OctalValue.MaxLength = 22;
+            this.OctalValue.Name = "OctalValue";
+            this.OctalValue.Size = new System.Drawing.Size(520, 26);
+            this.OctalValue.TabIndex = 6;
+            // 
+            // HexValue
+            // 
+            this.HexValue.Location = new System.Drawing.Point(190, 124);
+            this.HexValue.Margin = new System.Windows.Forms.Padding(4);
+            this.HexValue.MaxLength = 16;
+            this.HexValue.Name = "HexValue";
+            this.HexValue.Size = new System.Drawing.Size(520, 26);
+            this.HexValue.TabIndex = 7;
+            // 
+            // ValueType
+            // 
+            this.ValueType.FormattingEnabled = true;
+            this.ValueType.Items.AddRange(new object[] {
+            "Decimal",
+            "Binary",
+            "Octal",
+            "Hexadecimal"});
+            this.ValueType.Location = new System.Drawing.Point(22, 171);
+            this.ValueType.Margin = new System.Windows.Forms.Padding(4);
+            this.ValueType.Name = "ValueType";
+            this.ValueType.Size = new System.Drawing.Size(301, 26);
+            this.ValueType.TabIndex = 8;
+            this.ValueType.Text = "Please choose the starting format";
+            this.ValueType.SelectedIndexChanged += new System.EventHandler(this.startValueType_SelectedIndexChanged);
+            // 
+            // CalculateBttn
+            // 
+            this.CalculateBttn.Location = new System.Drawing.Point(330, 170);
+            this.CalculateBttn.Name = "CalculateBttn";
+            this.CalculateBttn.Size = new System.Drawing.Size(186, 26);
+            this.CalculateBttn.TabIndex = 9;
+            this.CalculateBttn.Text = "Calculate";
+            this.CalculateBttn.UseVisualStyleBackColor = true;
+            this.CalculateBttn.Click += new System.EventHandler(this.CalculateBttn_Click);
+            // 
+            // StatusReport
+            // 
+            this.StatusReport.AutoSize = true;
+            this.StatusReport.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.StatusReport.Location = new System.Drawing.Point(18, 211);
+            this.StatusReport.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.StatusReport.Name = "StatusReport";
+            this.StatusReport.Size = new System.Drawing.Size(101, 19);
+            this.StatusReport.TabIndex = 10;
+            this.StatusReport.Text = "Status report";
+            // 
+            // ClearBttn
+            // 
+            this.ClearBttn.Location = new System.Drawing.Point(524, 170);
+            this.ClearBttn.Name = "ClearBttn";
+            this.ClearBttn.Size = new System.Drawing.Size(186, 26);
+            this.ClearBttn.TabIndex = 11;
+            this.ClearBttn.Text = "Clear";
+            this.ClearBttn.UseVisualStyleBackColor = true;
+            this.ClearBttn.Click += new System.EventHandler(this.ClearBttn_Click);
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 243);
+            this.Controls.Add(this.ClearBttn);
+            this.Controls.Add(this.StatusReport);
+            this.Controls.Add(this.CalculateBttn);
+            this.Controls.Add(this.ValueType);
+            this.Controls.Add(this.HexValue);
+            this.Controls.Add(this.OctalValue);
+            this.Controls.Add(this.BinaryValue);
+            this.Controls.Add(this.DecimalValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "UserInterface";
+            this.Text = "Number Converter";
+            this.Load += new System.EventHandler(this.UserInterface_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox DecimalValue;
+        private System.Windows.Forms.TextBox BinaryValue;
+        private System.Windows.Forms.TextBox OctalValue;
+        private System.Windows.Forms.TextBox HexValue;
+        private System.Windows.Forms.ComboBox ValueType;
+        private System.Windows.Forms.Button CalculateBttn;
+        private System.Windows.Forms.Label StatusReport;
+        private System.Windows.Forms.Button ClearBttn;
+    }
+}
+
---
+++ NumberConverter/UserInterface.cs
@@ -0,0 +1,388 @@
+﻿/**
+ *  NumberConverter project
+ *  Made by Krzysztof Grzeslak 05.12.2016
+ * 
+ *  This is my final project for the CS50: Introduction to Computer Science MOOC.
+ *  
+ *  Program purpose is to convert between four number bases: Binary, Octal, Decimal and Hexadecimal
+ *  It includes simple User Interface based on the Windows Form
+ *  User receives status reports, if any error occurs.
+ *  
+ *  Note: I'm aware, that C# Libraries include methods, that would made this project much simpler.
+ *  But it was done the hard way (or reinventing the wheel way) for the educational purpose.
+ */
+
+using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NumberConverter
+{
+    enum status { noSource, noValue, wrongValue, reset, tooBigValue }
+    enum maxValIndex { maxDecimal, maxBinary, maxOctal, maxHex, Size }
+    enum numberType { n_decimal, n_binary, n_octal, n_hex }
+
+    struct sourceValue
+    {
+        public readonly string Text;
+        public readonly uint Divider;
+
+        public sourceValue(string text, uint divider) : this ()
+        {
+            Text = text;
+            Divider = divider;
+        }
+    }
+
+    public partial class UserInterface : Form
+    {
+        TextBox[] x_valueBoxes;
+        string[] x_maxValues; // [maxDecimal, maxBinary, maxOctal, maxHex]
+        Control x_activeControl;
+
+        public UserInterface()
+        {
+            InitializeComponent();
+            TextBox[] valueBoxes = { DecimalValue, BinaryValue, OctalValue, HexValue };
+            x_valueBoxes = valueBoxes;
+        }
+        
+        private void UserInterface_Load(object sender, EventArgs e)
+        {
+            initializeUI();
+        }
+
+        private void ClearBttn_Click(object sender, EventArgs e)
+        {
+            clearValues();
+        }
+
+        // Ensure user can only input selected source value
+        private void startValueType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            refreshForm(ValueType.Text);
+        }
+
+        private void CalculateBttn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (x_activeControl.Text == "")
+                {
+                    statusUpdate(status.noValue);
+                }
+                else
+                {
+                    statusUpdate(status.reset);
+                    string valType = ValueType.Text;
+                    convert(valType);
+                }
+
+            }
+            catch(System.OverflowException)
+            {
+                string valType = ValueType.Text;
+                reportBadValue(status.tooBigValue, valType);
+            }
+        }
+
+        // Main calculation roadmap
+        private void convert(string valType)
+        {
+            sourceValue srcVal;
+            switch (valType)
+            {
+                case "Binary":
+                    srcVal = new sourceValue(BinaryValue.Text, 2);
+                    break;
+
+                case "Octal":
+                    srcVal = new sourceValue(OctalValue.Text, 8);
+                    break;
+
+                case "Decimal":
+                    srcVal = new sourceValue(DecimalValue.Text, 10);
+                    break;
+
+                case "Hexadecimal":
+                    srcVal = new sourceValue(HexValue.Text.ToUpper(), 16);
+                    break;
+
+                default:
+                    statusUpdate(status.noSource);
+                    return;
+            }
+
+            if (isValueValid(srcVal.Text, srcVal.Divider))
+            {
+                string decVal = toDecimal(srcVal.Text, srcVal.Divider);
+                returnValues(decVal);
+            }
+            else
+            {
+                reportBadValue(status.wrongValue, valType);
+            }
+            this.ActiveControl = x_activeControl;
+        }
+
+        // Convert decimal number to other types. Reports false, if user input has incorrect format
+        private void returnValues(string decVal)
+        {
+            DecimalValue.Text = decVal;
+            BinaryValue.Text = fromDecimal(decVal, 2);
+            OctalValue.Text = fromDecimal(decVal, 8);
+            HexValue.Text = fromDecimal(decVal, 16);
+        }
+        
+        private void reportBadValue(status errorType, string valType)
+        {
+            statusUpdate(errorType, valType);
+            string curText = x_activeControl.Text;
+            clearValues();
+            x_activeControl.Text = curText;
+        }
+
+        // checks the decimal number for invalid characters
+        // divider: 2 for binary, 8 for octal, 10 for decimal and 16 for hex
+        private bool isValueValid(string input, uint divider)
+        {
+            bool isValid = true;
+            bool isHex = false;
+            char max = '\0';
+
+            if (divider < 10)
+            {
+                max = Convert.ToChar((uint)'0' + divider - 1);
+            }
+            else
+            {
+                max = '9';
+                isHex = true;
+            }
+
+            // Check if each character is valid
+            foreach (char c in input)
+            {
+                if (max == '\0' || c < '0' || c > max)
+                {
+                    if (isHex && c >= 'A' && c <= 'F')
+                    {
+                        // do nothing
+                    }
+                    else
+                    {
+                        isValid = false;
+                    }
+                } 
+            }
+            return isValid;
+        }
+        
+        // Converts decimal number, based on the given divider
+        private string fromDecimal(string decimalValue, uint divider)
+        {
+            StringBuilder newValue = new StringBuilder("");
+            ulong decValue = ulong.Parse(decimalValue);
+            uint nextDigit = 0;
+            do
+            {
+                nextDigit = (uint)(decValue % (divider));
+                if (nextDigit > 9)
+                {
+                    newValue.Insert(0, toHexLetter(nextDigit));
+                }
+                else
+                {
+                    newValue.Insert(0, nextDigit);
+                }
+                decValue /= divider;
+            }
+            while (decValue > 0);
+            return newValue.ToString();
+        }
+
+        // Converts other number types to decimal number
+        private string toDecimal(string curVal, uint numberBase)
+        {
+            // check if value is already decimal
+            if (numberBase == 10)
+            {
+                return curVal;
+            }
+
+            ulong decValue = 0;
+            uint nextDigit = 0;
+
+            // Process each digit
+            for (int i = curVal.Length - 1, j = 0; i >= 0; i--, j++)
+            {
+                nextDigit = getNextDigit(curVal[i]);
+                checked
+                {
+                    decValue += (ulong)nextDigit * (ulong)Math.Pow(numberBase, (double)j);
+                }
+            }
+
+            return decValue.ToString();
+        }
+
+        // Determine next digit
+        private uint getNextDigit (char nextChar)
+        {
+            uint nextDigit = 0;
+            // check if digit is hex bigger than 9
+            if (nextChar >= 'A' && nextChar <= 'F')
+            {
+                nextDigit = fromHexLetter(nextChar);
+            }
+            else
+            {
+                nextDigit = (uint)Char.GetNumericValue(nextChar);
+            }
+            return nextDigit;
+        }
+
+        // Converts 10-15 digits to A-F
+        private char toHexLetter(uint digit)
+        {
+            char retVal = Convert.ToChar(Convert.ToInt32('A') + digit - 10);
+            return retVal;
+        }
+
+        // Converts A-F to 10-15 digits
+        private uint fromHexLetter(char hexDigit)
+        {
+            char retVal = Convert.ToChar(Convert.ToInt32(hexDigit - 'A') +  10);
+            return retVal;
+        }
+
+        // unlock the field for selected input value type
+        private void refreshForm(string valType)
+        {
+            lockAllFields();
+            clearValues();
+
+            TextBox activeBox = null;
+            switch (valType)
+            {
+                case "Decimal":
+                    activeBox = DecimalValue;
+                    break;
+
+                case "Binary":
+                    activeBox = BinaryValue;
+                    break;
+
+                case "Octal":
+                    activeBox = OctalValue;
+                    break;
+
+                case "Hexadecimal":
+                    activeBox = HexValue;
+                    break;
+
+                default:
+                    // do nothing
+                    break;
+            }
+            activeBox.ReadOnly = false;
+            x_activeControl = activeBox;
+            this.ActiveControl = x_activeControl;
+        }
+
+        private void initializeUI()
+        {
+            setMaxValues();
+            lockAllFields();
+            this.ActiveControl = ValueType;
+            x_activeControl = this.ActiveControl;
+            statusUpdate(status.noSource);
+        }
+
+        private void clearValues()
+        {
+            foreach(TextBox valueBox in x_valueBoxes)
+            {
+                valueBox.Text = "";
+            }
+            this.ActiveControl = x_activeControl;
+        }
+
+        private void lockAllFields()
+        {
+            foreach (TextBox valueBox in x_valueBoxes)
+            {
+                valueBox.ReadOnly = true;
+            }
+        }
+
+        // Provides error feedback to the user
+        private void statusUpdate(status state, string numberType = "")
+        {
+            string message = "";
+            switch (state)
+            {
+                case status.wrongValue:
+                    message = String.Format("{0} number is not valid. Please try again.", numberType);
+                    break;
+
+                case status.noValue:
+                    message = "Please input number to convert";
+                    break;
+
+                case status.noSource:
+                    message = "Please choose the source format";
+                    break;
+
+                case status.tooBigValue:
+                    string maxValue = getMaxValue(numberType);
+                    message = String.Format( "Max value is : {0}", maxValue);
+                    break;
+
+                default:
+                case status.reset:
+                    message = "";
+                    break;
+            }
+
+            StatusReport.Text = message;
+        }
+
+        // Determine max value for each number type
+        private void setMaxValues()
+        {
+            string maxDecimal = ulong.MaxValue.ToString();
+            string[] maxValues = new string[(int)maxValIndex.Size];
+            maxValues[(int)maxValIndex.maxDecimal] = maxDecimal;
+            maxValues[(int)maxValIndex.maxBinary] = fromDecimal(maxDecimal, 2).ToString();
+            maxValues[(int)maxValIndex.maxOctal] = fromDecimal(maxDecimal, 8).ToString();
+            maxValues[(int)maxValIndex.maxHex] = fromDecimal(maxDecimal, 16).ToString();
+
+            x_maxValues = maxValues;
+        }
+
+        string getMaxValue(string valType)
+        {
+            string maxValue = "";
+            switch (valType)
+            {
+                case "Decimal":
+                    maxValue = x_maxValues[(int)maxValIndex.maxDecimal];
+                    break;
+
+                case "Binary":
+                    maxValue = x_maxValues[(int)maxValIndex.maxBinary];
+                    break;
+
+                case "Octal":
+                    maxValue = x_maxValues[(int)maxValIndex.maxOctal];
+                    break;
+
+                case "Hexadecimal":
+                    maxValue = x_maxValues[(int)maxValIndex.maxHex];
+                    break;
+            }
+            return maxValue;
+        }
+    }
+}
